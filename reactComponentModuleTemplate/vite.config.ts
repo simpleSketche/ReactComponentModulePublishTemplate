cbf71cc1ab5322abbc1701828fc3f1f7d0633f14
SimpleSketch_Yankun <yangyankun.work@gmail.com>
2024-09-30 14:44:33 -0400

    update

--- reactComponentModuleTemplate/vite.config.ts
+++ reactComponentModuleTemplate/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      name: "ReactComponentmodulePublishTemplate",
+      fileName: "ReactComponentmodulePublishTemplate",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "react",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "react/jsx-runtime",
+        },
+      },
+    },
+  },
+});
